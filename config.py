b04a555083e13700a735ada0776efc9851e9d5cf
AllSystem88 <deependray228@gmail.com>
2026-01-14 11:49:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,11 +16,9 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-# API URL - here
-API_URL = getenv("API_URL", "https://beta.fallenapi.fun")
-
-# API KEY - here
-API_KEY = getenv("API_KEY", None) #for api contact @its_deep_ydv
+# Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
+YT_API_KEY = getenv("YT_API_KEY" , None ) 
 
 # For - downloads
 DOWNLOADS_DIR = "downloads"
